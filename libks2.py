1073a3224cce5d0cc72fc23ff7c54ec3d55068a7
Zhichao Fang (mac) <zhichaofang@outlook.com>
2018-01-13 23:03:09 +0800

    Add copyright

--- libks2.py
+++ libks2.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+##########################################################################
+# libks2: A python module for accessing Kyowa KS2 file for EDX-2000A     #
+# Copyright (C) 2018 ZC. Fang (zhichaofang@sjtu.org)                     #
+#                                                                        #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+##########################################################################
 
 import struct
 import numpy as np
@@ -289,12 +306,13 @@ class KS2:
 
 if __name__ == "__main__":
     print("""
-A python code for converting ks2 to mat.
-    By Dr. ZC Fang (Copyright 2018)
+A Python code to access Kyowa KS2 file.
+Copyright (C) 2018 ZC. Fang (zhichaofang@sjtu.org)
+
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it under certain conditions.
         """)
 
-    ks2 = KS2('test.ks2')
+    ks2 = KS2('example.ks2')
     print(ks2)
     print(ks2.raw)
-    ks2.convertData()
-    print(ks2.data)
